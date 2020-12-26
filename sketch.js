054e17fce2c7fc469cc6a8ca8b40bbecfd519a7d
Vaibhavi <vaibhavi.khyadi@gmail.com>
2020-12-26 09:45:21 +0530

    added the chain

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6, chain
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,7 +36,12 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    log6 =  new Log(150,180,100, PI/2);
+
     bird = new Bird(100,100);
+
+chain = new Chain (bird.body , log6.body)
+    
 
 }
 
@@ -60,5 +67,8 @@ function draw(){
     log5.display();
 
     bird.display();
+    log6.display();
     platform.display();
+    chain.display();
+    
 }
